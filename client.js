5be943d35e74860f7d765182ee0f73bc54a392ea
zoulhafi <58788411+zoulhafi@users.noreply.github.com>
2020-03-19 15:13:08 +0100

    updated ip

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-const 	connection = new WebSocket('wss://api.demo-oulhafiane.me:443');
+const 	connection = new WebSocket('wss://139.59.215.10:443');
 const 	loginPage = document.querySelector('#login-page');
 const 	usernameInput = document.querySelector('#username');
 const 	loginButton = document.querySelector('#login');
